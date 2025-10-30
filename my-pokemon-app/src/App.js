db9a6e0072f99854b2c0a4061af92a09fdb6fd79
kim.luke <luke.kimyh@gmail.com>
2025-10-29 19:02:23 -0500

    added working button and search field

--- my-pokemon-app/src/App.js
+++ my-pokemon-app/src/App.js
@@ -1,32 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { TextField, Grid, Button, Box } from '@mui/material';
 
 function App() {
 
-  const [backEndData, setBackEndData] = useState(null);
+  const [pokemonData, setPokemonData] = useState(null);
+  const [searchValue, setSearchValue] = useState('');
 
-  const callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await callPokemonAPI();
+  }
 
-    if (response.status !== 200) {
-      throw Error(body.message) 
+  const handleChange = (event) => {
+    setSearchValue(event.target.value);
+  }
+
+  const callPokemonAPI = async () => {
+    const pokemonName = searchValue;
+
+    try {
+      const response = await fetch(`/pokemon/${pokemonName}`);
+      if (!response.ok) {
+        throw new Error('Pokemon not found!');
+      }
+
+      const data = await response.json();
+      setPokemonData(data);
+      debugger;
+
+    } catch (err) {
+      console.log('Pokemon not found :(')
+      setPokemonData(null);
     }
-    return body;
   };
-
-  useEffect(() => {
-    callBackendAPI()
-      .then(res => setBackEndData(res.express))
-      .catch(err => console.log(err));
-  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        Pokemon Finder
-      </header>
+      <Grid container spacing={2} justifyContent={'center'}>
+        <Grid item size={12} textAlign={'center'}>
+          <h1>Find a Pokemon!</h1>
+        </Grid>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{ display: "flex", flexDirection: "row", gap: 2, width: 300 }}
+        >
+          <TextField
+            id="filled-search"
+            type="search"
+            variant="filled"
+            label="Enter a Pokemon's name"
+            value={searchValue}
+            onChange={handleChange}
+            fullWidth
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Search
+          </Button>
+        </Box>
+      </Grid>
     </div>
   );
 }
--- pokemon.js
+++ pokemon.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// create a GET route
+app.get('/pokemon/:name', async (req, res) => {
+  const {name} = req.params;
+
+  try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(404).json({ error: 'Pokémon not found' });
+    }
+});
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
